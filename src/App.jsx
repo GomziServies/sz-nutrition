05fdfa5cddbf8a8895514cc5e5bd745431360eed
Yash Sutariya <84495766+yash-sutariya@users.noreply.github.com>
2025-05-14 09:47:48 +0530

    bug solve and design change

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import { Route, Routes } from "react-router-dom";
 
 //User Account
 import Home from "./pages/home";
-import AddToCart from "./pages/nutrition/add-to-cart";
 import PureGoWheyProtein from "./pages/products/whey-protein-powder";
 import NotFoundPage from "./pages/404";
-import CheckOut from "./pages/check-out";
 import ScrollRestoration from "./components/scroll-restoration";
 import AboutUs from "./pages/nutrition/about-us";
 import ContactUs from "./pages/nutrition/contact-us";
@@ -16,8 +14,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/add-to-cart" element={<AddToCart />} />
-        <Route path="/check-out" element={<CheckOut />} />
         {/* Products */}
         <Route path="/whey-protein-powder" element={<PureGoWheyProtein />} />
         <Route path="/about-us" element={<AboutUs />} />
--- src/components/CourseButtonsContainer.jsx
+++ src/components/CourseButtonsContainer.jsx
@@ -1,32 +1,7 @@
 import React from "react";
-import AddtoCartOffCourseCanvas from "./addtocartcoursecanvas.jsx";
 import { Link } from "react-router-dom";
 
-const CourseButtonsContainer = ({
-  currentProductData,
-  menuOpen,
-  setMenuOpen,
-}) => {
-  const toggleMenu = async (currentProductData, e) => {
-    try {
-      const existingData = JSON.parse(
-        localStorage.getItem("addItemInCart")
-      ) || { products: [] };
-
-      const isProductExists = existingData.products.some(
-        (item) => item.name === currentProductData.name
-      );
-
-      if (!isProductExists) {
-        existingData.products.push(currentProductData);
-        localStorage.setItem("addItemInCart", JSON.stringify(existingData));
-      }
-      setMenuOpen(!menuOpen);
-    } catch (error) {
-      console.error("Error adding to cart:", error);
-    }
-  };
-
+const CourseButtonsContainer = () => {
   return (
     <div className="col-12 p-0">
       <div className="m-0 w-100 py-2 pt-3 px-0">
